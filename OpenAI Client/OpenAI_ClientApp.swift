33e226756201589fdfeae5c88b39b283919b12c8
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-01-27 16:37:48 +0100

    Project Created

---
+++ OpenAI Client/OpenAI_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAI_ClientApp.swift
+//  OpenAI Client
+//
+//  Created by Nunzio Ricci on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenAI_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
